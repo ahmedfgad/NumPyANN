8ecd7fe4e57ddf737bb9c3e671d9737af4908481
Ahmed Gad <ahmed.f.gad@gmail.com>
2019-01-18 18:06:29 +0200

    Add files via upload

---
+++ ann_numpy.py
@@ -0,0 +1,85 @@
+import numpy
+import pickle
+
+def sigmoid(inpt):
+    return 1.0 / (1 + numpy.exp(-1 * inpt))
+
+def relu(inpt):
+    result = inpt
+    result[inpt < 0] = 0
+    return result
+
+def update_weights(weights, learning_rate):
+    new_weights = weights - learning_rate * weights
+    return new_weights
+
+def train_network(num_iterations, weights, data_inputs, data_outputs, learning_rate, activation="relu"):
+    for iteration in range(num_iterations):
+        print("Itreation ", iteration)
+        for sample_idx in range(data_inputs.shape[0]):
+            r1 = data_inputs[sample_idx, :]
+            for idx in range(len(weights) - 1):
+                curr_weights = weights[idx]
+                r1 = numpy.matmul(a=r1, b=curr_weights)
+                if activation == "relu":
+                    r1 = relu(r1)
+                elif activation == "sigmoid":
+                    r1 = sigmoid(r1)
+            curr_weights = weights[-1]
+            r1 = numpy.matmul(a=r1, b=curr_weights)
+            predicted_label = numpy.where(r1 == numpy.max(r1))[0][0]
+            desired_label = data_outputs[sample_idx]
+            if predicted_label != desired_label:
+                weights = update_weights(weights,
+                                         learning_rate=0.001)
+    return weights
+
+def predict_outputs(weights, data_inputs, activation="relu"):
+    predictions = numpy.zeros(shape=(data_inputs.shape[0]))
+    for sample_idx in range(data_inputs.shape[0]):
+        r1 = data_inputs[sample_idx, :]
+        for curr_weights in weights:
+            r1 = numpy.matmul(a=r1, b=curr_weights)
+            if activation == "relu":
+                r1 = relu(r1)
+            elif activation == "sigmoid":
+                r1 = sigmoid(r1)
+        predicted_label = numpy.where(r1 == numpy.max(r1))[0][0]
+        predictions[sample_idx] = predicted_label
+    return predictions
+
+f = open("dataset_features.pkl", "rb")
+data_inputs2 = pickle.load(f)
+f.close()
+
+features_STDs = numpy.std(a=data_inputs2, axis=0)
+data_inputs = data_inputs2[:, features_STDs > 50]
+
+f = open("outputs.pkl", "rb")
+data_outputs = pickle.load(f)
+f.close()
+
+HL1_neurons = 150
+input_HL1_weights = numpy.random.uniform(low=-0.1, high=0.1,
+                                         size=(data_inputs.shape[1], HL1_neurons))
+HL2_neurons = 60
+HL1_HL2_weights = numpy.random.uniform(low=-0.1, high=0.1,
+                                       size=(HL1_neurons, HL2_neurons))
+output_neurons = 4
+HL2_output_weights = numpy.random.uniform(low=-0.1, high=0.1,
+                                          size=(HL2_neurons, output_neurons))
+
+weights = numpy.array([input_HL1_weights,
+                       HL1_HL2_weights,
+                       HL2_output_weights])
+
+weights = train_network(num_iterations=10,
+                        weights=weights,
+                        data_inputs=data_inputs,
+                        data_outputs=data_outputs,
+                        learning_rate=0.01,
+                        activation="relu")
+
+predictions = predict_outputs(weights, data_inputs)
+num_flase = numpy.where(predictions != data_outputs)[0]
+print("num_flase ", num_flase.size)
---
+++ extract_features.py
@@ -0,0 +1,29 @@
+import numpy
+import skimage.io, skimage.color, skimage.feature
+import os
+import pickle
+
+fruits = ["apple", "raspberry", "mango", "lemon"]
+#492+490+490+490=1,962
+dataset_features = numpy.zeros(shape=(1962, 360))
+outputs = numpy.zeros(shape=(1962))
+
+idx = 0
+class_label = 0
+for fruit_dir in fruits:
+    curr_dir = os.path.join(os.path.sep, fruit_dir)
+    all_imgs = os.listdir(os.getcwd()+curr_dir)
+    for img_file in all_imgs:
+        fruit_data = skimage.io.imread(fname=os.getcwd()+curr_dir+'\\'+img_file, as_grey=False)
+        fruit_data_hsv = skimage.color.rgb2hsv(rgb=fruit_data)
+        hist = numpy.histogram(a=fruit_data_hsv[:, :, 0], bins=360)
+        dataset_features[idx, :] = hist[0]
+        outputs[idx] = class_label
+        idx = idx + 1
+    class_label = class_label + 1
+
+with open("dataset_features.pkl", "wb") as f:
+    pickle.dump("dataset_features.pkl", f)
+
+with open("outputs.pkl", "wb") as f:
+    pickle.dump(outputs, f)

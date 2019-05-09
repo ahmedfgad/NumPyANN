000e7b0a04003d26408da322bf70c1c8239da335
Ahmed Gad <ahmed.f.gad@gmail.com>
2019-05-09 13:18:34 +0200

    Update extract_features.py
    The d=features array was not passed correctly to the dump() function. The previous line pickle.dump("dataset_features.pkl", f), where the array name was mistakenly written as a string file name with its extension, is replaced by pickle.dump(dataset_features, f) where the array name dataset_features is passed correctly.

--- extract_features.py
+++ extract_features.py
@@ -14,16 +14,17 @@ for fruit_dir in fruits:
     curr_dir = os.path.join(os.path.sep, fruit_dir)
     all_imgs = os.listdir(os.getcwd()+curr_dir)
     for img_file in all_imgs:
-        fruit_data = skimage.io.imread(fname=os.getcwd()+curr_dir+'\\'+img_file, as_grey=False)
-        fruit_data_hsv = skimage.color.rgb2hsv(rgb=fruit_data)
-        hist = numpy.histogram(a=fruit_data_hsv[:, :, 0], bins=360)
-        dataset_features[idx, :] = hist[0]
-        outputs[idx] = class_label
-        idx = idx + 1
+        if img_file.endswith(".jpg"): # Ensures reading only JPG files.
+            fruit_data = skimage.io.imread(fname=os.getcwd()+curr_dir+'\\'+img_file, as_grey=False)
+            fruit_data_hsv = skimage.color.rgb2hsv(rgb=fruit_data)
+            hist = numpy.histogram(a=fruit_data_hsv[:, :, 0], bins=360)
+            dataset_features[idx, :] = hist[0]
+            outputs[idx] = class_label
+            idx = idx + 1
     class_label = class_label + 1
 
 with open("dataset_features.pkl", "wb") as f:
-    pickle.dump("dataset_features.pkl", f)
+    pickle.dump(dataset_features, f)
 
 with open("outputs.pkl", "wb") as f:
     pickle.dump(outputs, f)
